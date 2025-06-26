a4c9412896502a53bed2005040a7422d114cbffd
luisgustavo2012 <lgroquedesiderio@gmail.com>
2025-06-26 20:31:44 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-alert("Comando para mostrar um texto na tela, posso usar aspas duplas ou aspas simples, mas siga este padrão em todo codigo")
-alert("Isto é um alerta para poder te alertar sobre um alertaque ira te alertar em seguida a esse alerto")
-alert("ALERTA")
+alert("Comando para mostrar um texto na tela, posso usar aspas duplas ou aspas simples, mas siga este padrão em todo codigo");
+alert("Isto é um alerta para poder te alertar sobre um alertaque ira te alertar em seguida a esse alerto");
+alert("ALERTA");
 
 let numero = 25;
 let chute;
