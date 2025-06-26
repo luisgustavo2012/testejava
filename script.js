196496562bed94b29edce0838f6adcfe9cabd6c3
luisgustavo2012 <lgroquedesiderio@gmail.com>
2025-06-26 20:58:32 -0300

    .

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 alert('Seja bem-vindo ao nosso jogo!');
 
 // Gera um número secreto aleatório entre 1 e 10
-let numeroSecreto = 5;
+let numeroSecreto = 5; // Você pode usar Math.floor(Math.random() * 10) + 1
 console.log(numeroSecreto); // (Para testes, pode remover depois)
 
-// Variável para armazenar o chute do usuário
-let primeiroNumero = prompt("Digite um número:");
-let segundoNumero = prompt("Digite um número:");
-let terceiroNumero = prompt("Digite um número:");
-let quartoNumero = prompt("Digite um número:");
-let media;
-let mediaTotal;
+// Variável para armazenar os chutes do usuário
+let primeiroNumero = Number(prompt("Digite o primeiro número:"));
+let segundoNumero = Number(prompt("Digite o segundo número:"));
+let terceiroNumero = Number(prompt("Digite o terceiro número:"));
+let quartoNumero = Number(prompt("Digite o quarto número:"));
 
-media = primeiroNumero + segundoNumero + terceiroNumero + quartoNumero;
-mediaTotal = media /4;
+// Cálculo da média
+let media = primeiroNumero + segundoNumero + terceiroNumero + quartoNumero;
+let mediaTotal = media / 4;
 
-  if (mediaTotal == numeroSecreto) {
-    alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} na tentativa.`);
-  } else if (mediaTotal > numeroSecreto) {
-    alert(`O número secreto é menor que ${mediaTotal}. Tente novamente.`);
-  } else {
-    alert(`O número secreto é maior que ${mediaTotal}. Tente novamente.`);
-  }
+// Verificação do resultado
+if (mediaTotal == numeroSecreto) {
+  alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} com a média.`);
+} else if (mediaTotal > numeroSecreto) {
+  alert(`O número secreto é menor que a média ${mediaTotal}. Tente novamente.`);
+} else {
+  alert(`O número secreto é maior que a média ${mediaTotal}. Tente novamente.`);
+}
