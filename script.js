85ec3a19a5fb6f85370eca6631f2b86fc060fabb
luisgustavo2012 <lgroquedesiderio@gmail.com>
2025-06-26 20:29:12 -0300

    .

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 alert("Comando para mostrar um texto na tela, posso usar aspas duplas ou aspas simples, mas siga este padrão em todo codigo")
+alert("Isto é um alerta para poder te alertar sobre um alertaque ira te alertar em seguida a esse alerto")
+alert("ALERTA")
+
 let numero = 25;
 let chute;
 let idade;
-let nuome;
+let nome = console.log(prompt);
 
 console.log (numero);
-alert("Isto é um alerta para poder te alertar sobre um alertaque ira te alertar em seguida a esse alerto")
-alert("ALERTA")
+console.log(prompt ("Digite seu nome");) 
+
