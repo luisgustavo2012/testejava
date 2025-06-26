1c9f53c86fb7b918240f417bb2ec3a95e4da7279
luisgustavo2012 <lgroquedesiderio@gmail.com>
2025-06-26 20:20:45 -0300

    .

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 alert("Comando para mostrar um texto na tela, posso usar aspas duplas ou aspas simples, mas siga este padrão em todo codigo")
-alert("Ele é limitado ao clique nao ok pelo usuario")
-alert("Não é possivel configurar seu tempo de permanencia na tela")
+let numero = 25;
+let chute;
+let idade;
+let nuome;
+
+console.log (numero);
