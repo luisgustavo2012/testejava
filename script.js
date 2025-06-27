ef9ddba44caf8444320162ffcd8f0d2fb9aafab1
luisgustavo2012 <lgroquedesiderio@gmail.com>
2025-06-26 21:06:39 -0300

    .

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 alert('Seja bem-vindo ao nosso jogo!');
 
-// Gera um número secreto aleatório entre 1 e 10
-let numeroSecreto = 5; // Você pode usar Math.floor(Math.random() * 10) + 1
-console.log(numeroSecreto); // (Para testes, pode remover depois)
-
-// Variável para armazenar os chutes do usuário
 let primeiroNumero = Number(prompt("Digite o primeiro número:"));
 let segundoNumero = Number(prompt("Digite o segundo número:"));
 let terceiroNumero = Number(prompt("Digite o terceiro número:"));
@@ -14,11 +9,5 @@ let quartoNumero = Number(prompt("Digite o quarto número:"));
 let media = primeiroNumero + segundoNumero + terceiroNumero + quartoNumero;
 let mediaTotal = media / 4;
 
-// Verificação do resultado
-if (mediaTotal == numeroSecreto) {
-  alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} com a média.`);
-} else if (mediaTotal > numeroSecreto) {
-  alert(`O número secreto é menor que a média ${mediaTotal}. Tente novamente.`);
-} else {
-  alert(`O número secreto é maior que a média ${mediaTotal}. Tente novamente.`);
-}
+
+  alert(`A média é ${mediaTotal}.`);
